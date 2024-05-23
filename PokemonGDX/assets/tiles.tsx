cef003c8918b03825ae3fc20606d0fdc35a5ab27
ErnestoGUFI <159938654+ErnestoGUFI@users.noreply.github.com>
2024-05-22 19:17:17 -0700

    actualizacion(Colisiones,sprites,objetos)

---
+++ PokemonGDX/assets/tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="d36bih3-7bd0f401-eaaa-493a-b102-e5abbcb3bc67 (1)" tilewidth="16" tileheight="16" tilecount="272" columns="8">
+ <image source="../../../Downloads/d36bih3-7bd0f401-eaaa-493a-b102-e5abbcb3bc67 (1).png" width="128" height="544"/>
+</tileset>
--- PokemonGDX/core/src/com/pokemon/game/MyPokemonGame.java
+++ PokemonGDX/core/src/com/pokemon/game/MyPokemonGame.java
@@ -1,24 +1,91 @@
 package com.pokemon.game;
 
-import com.badlogic.gdx.Game;
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.pokemon.game.screens.GameScreen;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
+import Maps.Mapa;
+import Player.Controles;
+import Player.Jugador;
 
-public class MyPokemonGame extends Game {
+public class MyPokemonGame extends ApplicationAdapter {
 
-    public SpriteBatch batch;
-
+    private SpriteBatch batch;
+    private OrthographicCamera camera;
+    private Viewport viewport;
+    private Jugador jugador;
+    private Controles controles;
+    private Mapa[] mapas;
+    private int mapaActualIndex;
+    
     @Override
     public void create () {
         batch = new SpriteBatch();
-        this.setScreen(new GameScreen(this));
+        camera = new OrthographicCamera();
+        viewport = new FitViewport(640, 300, camera);
+        viewport.apply();
+        
+        jugador = new Jugador();
+        controles = new Controles();
+
+        
+        mapas = new Mapa[] {
+            new Mapa("mapa.tmx", camera),
+            new Mapa("mapa2.tmx", camera),
+            
+        };
+        mapaActualIndex = 0;
+
+        Gdx.input.setInputProcessor(controles);
     }
     
     @Override
     public void render () {
-    	super.render();
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        camera.position.set(jugador.x, jugador.y, 0);
+        camera.update();
+
+        mapas[mapaActualIndex].render();
+        
+        jugador.update(controles, mapas[mapaActualIndex].getTiledMap(), this);
+
+        batch.setProjectionMatrix(camera.combined);
+        batch.begin();
+        jugador.dibujar(batch, controles);
+        batch.end();
+        
+        System.out.println(jugador.x+" "+jugador.y);
     }
 
-}
+    @Override
+    public void resize(int width, int height) {
+        viewport.update(width, height);
+        camera.position.set(camera.viewportWidth / 2, camera.viewportHeight / 2, 0);
+        mapas[mapaActualIndex].resize(width, height);
+    }
 
+    @Override
+    public void dispose () {
+        batch.dispose();
+        jugador.dispose();
+        for (Mapa mapa : mapas) {
+            mapa.dispose();
+        }
+    }
+
+    public void cambiarMapa(int nuevoIndice, float nuevaPosX, float nuevaPosY) {
+        if (nuevoIndice >= 0 && nuevoIndice < mapas.length) {
+            mapaActualIndex = nuevoIndice;
+            jugador.setPosition(nuevaPosX, nuevaPosY);
+        }
+    }
+}
