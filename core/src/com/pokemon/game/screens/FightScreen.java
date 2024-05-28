e285b575b4d660ed6837fd5dc16d891f09df5398
s3bas06 <sebastianoa06@gmail.com>
2024-05-28 09:41:01 -0700

    Actualizacion de la clase FightScreen

--- core/src/com/pokemon/game/screens/FightScreen.java
+++ core/src/com/pokemon/game/screens/FightScreen.java
@@ -7,9 +7,11 @@ import javax.swing.Timer;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
@@ -40,6 +42,8 @@ public class FightScreen implements Screen{
     //Timer que llevara el tiempo del juego.
     Timer tiempo;
     int seg=0;
+    //Objeto de la clase ShapeRenderer que dibuja formas.
+    ShapeRenderer sr;
     
     
     public FightScreen(MyPokemonGame game) {
@@ -49,12 +53,15 @@ public class FightScreen implements Screen{
     	 pokemonEnemigoSprite = new Texture("squirtleSprite.png");
     	 vsSprite = new Texture("vsSprite.png");
     	 fondoBatalla = new Texture("fondoBatalla.png");
+    	 
     	 tiempo = new Timer(1000, new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				seg++;
 			}
     	 });
+    	 
+    	 sr = new ShapeRenderer();
     }
     
 	@Override
@@ -92,6 +99,7 @@ public class FightScreen implements Screen{
             	ySpriteEnemigo--;
             	System.out.println(ySpriteEnemigo);
             }
+            game.batch.end();
         }
         
         if(seg>=2) {
@@ -99,17 +107,35 @@ public class FightScreen implements Screen{
         	Gdx.gl.glClearColor(240, 240, 240, 1);
         	Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
             
+        	//Dibujar utilizando el batch.
         	game.batch.draw(fondoBatalla, 48, 47, 1178, 630);
         	
+        	game.batch.end();
+        	
+        	//Dibujar utilizando el ShapeRenderer
+        	sr.begin(ShapeRenderer.ShapeType.Filled);
+        	sr.setColor(new Color(217/255f, 217/255f, 217/255f, 0.8f)); //Se divide entre 255 ya que el constructor no acepta valores mayores a 1.
+        	sr.rect(48, 47, 1178, 150);
+        	
+        	roundRect(sr, 90, 63, 520, 120, 20, new Color(175/255f, 175/255f, 175/255f,0.8f));
+        	
+        	//Rectangulos del ataque.
+        	roundRect(sr,695,130,210,52,10,new Color(255/255f, 0/255f, 0/255f,0.8f));
+        	roundRect(sr,965,130,210,52,10,new Color(255/255f, 0/255f, 0/255f,0.8f));
+        	roundRect(sr,695,58,210,52,10,new Color(255/255f, 0/255f, 0/255f,0.8f));
+        	roundRect(sr,965,58,210,52,10,new Color(255/255f, 0/255f, 0/255f,0.8f));
+        	
+        	//Rectangulo de ataque seleccionado.
+        	roundRect(sr,698,135,204,42,6,new Color(248/255f, 168/255f, 176/255f,0.8f)); //Este ira cambiando.
+        	
+        	sr.end();
         }
         
         
         
         
         
-        game.batch.end();
         
-    		
 	}
 
 	@Override
@@ -135,6 +161,27 @@ public class FightScreen implements Screen{
 	@Override
 	public void dispose() {
         game.batch.dispose();
+        if(seg>=2) sr.dispose();
+	}
+	
+	public void roundRect(ShapeRenderer sr, int x, int y, int width, int height, int radio, Color color) {
+		sr.setColor(color);
+		
+		sr.rect(x, y, width, height);
+		
+		//Esquina inferior izquierda
+		sr.arc(x, y + radio, radio, 180, 90);
+		//Esquina superior izquierda
+		sr.arc(x, y + height - radio, radio, 90, 90);
+		
+		//Esquina inferior derecha
+		sr.arc(x + width, y + radio, radio, 270, 90);
+		//Esquina superior derecha
+		sr.arc(x + width, y + height - radio, radio, 0, 90);
+		
+		sr.rect(x - radio, y + radio, radio, height - (2*radio));
+		sr.rect(x + width, y + radio, radio, height - (2*radio));
+	
 	}
 	
 }
