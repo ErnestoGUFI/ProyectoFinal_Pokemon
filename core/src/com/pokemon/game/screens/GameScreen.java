c269a6ed9f784aa93a07293dc2e78c8930b47812
ArielPerez2001 <felixperez230@gmail.com>
2024-06-04 01:02:04 -0700

    PauseScreen
    Pendiente guardar y salir

--- core/src/com/pokemon/game/screens/GameScreen.java
+++ core/src/com/pokemon/game/screens/GameScreen.java
@@ -53,6 +53,9 @@ public class GameScreen implements Screen{
 
 	@Override
 	public void render(float delta) {
+		if (Gdx.input.isKeyJustPressed(com.badlogic.gdx.Input.Keys.ESCAPE)) {
+            game.setScreen(new PauseScreen(game, this));
+        }
 		
 		Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
